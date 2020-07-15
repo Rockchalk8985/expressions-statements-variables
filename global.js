65f0e5b77c6fa5ad3535c4f4ca50bfb4ed12fd8a
Rockchalk8985 <steven.foster10@gmail.com>
2020-07-15 14:33:54 -0500

    finished project

--- global.js
+++ global.js
@@ -7,36 +7,47 @@ console.log(4 + 5);
 
 //1.) Log an expression that evaluates to the sum of two integers
 // your code here
-console.log("answer 1");
+
+console.log(14 + 14);
 
 // 2.) log an expression that evaluates to the difference of two integers
 // your code here
-console.log("answer 2");
+console.log(14 - 11);
 
 // 3.) Log an expression that concatenates the two strings "I'm writing" and "my first JavaScript code"
 // your code here
-console.log("answer 3");
+
+console.log ("I'm writing"  + " " + "my first JavaScript code");
 
 // 4.) Log an expression that evaluates to true or false using two numbers that are greater or less than eachother
 // your code here
-console.log("answer 4");
+let number1 = 4
+let number2 = 6
+
+console.log(number1 < number2);
 
 // 5.) Log an expression that uses modulo, with a result of 4
 // your code here
-console.log("answer 5");
+let number3 = 24;
+let number4 = 5;
+
+console.log(number3 % number4);
 
 // 6.) Create two different strings and compare them, logging the result.
 // It should be false!
-console.log("answer 6");
+let str1 = "I'm the best";
+let str2 = "No your not";
+
+console.log(str1 === str2);
 
 // 7.) Concatenate 4 different strings to form the sentence "I'm learning a bit of JavaScript at Kenzie"
-console.log("answer 7");
+console.log("I'm learning" + " " + "a bit of" + " " +"Javascript" + " " + "at Kenzie");
 
 // 8.) Uncomment this code and create an expression inside of it.
-// The expression should represent: 5 is less than 10 and 8 is greater than 5
+// The expression should represent: 5 < 10 and 8 > 5
 // This should print true
 
-// console.log("answer 8")
+console.log(5 < 10 && 8 > 5)
 
 // 9.) Add parenthesis to the following statement, the result should be 1
-console.log(3 * 3 + 1 / 20 / 2 == 1); // This should log true
+console.log((3 * 3 + 1) / (20 / 2) == 1); // This should log true
